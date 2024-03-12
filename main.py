89fb89a3a0e5d0542c607e65ff60ad4ccd2c27e0
Bholi <najmulbro123@gmail.com>
2024-03-12 21:03:06 +0545

    Easier method

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 print('Sum of all Even Numbers between 1 to 100 !')
 
 s = 0
-for i in range(1, 101):
-    if i % 2 == 0:
-        # print(i)
-        s += i
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#         # print(i)
+#         s += i
+
+# Easier Method
+for i in range(2, 101, 2):
+    s += i
+
 print(f'The sum of the even numbers is {s}')
